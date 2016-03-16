9ecef76a1a2b0500e14398b5c721867eb8866737
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-03-16 11:15:06 -0700

    adding code

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// print to the developer console to makesure javascript is working!
+console.log('javascript is working');
+
+/* ask the user their name */
+var userName = prompt('Thank you for wanting to learn more about me! What is your name?');
+
+/* greet the user by name */
+alert('Hi ' + userName + ', I have a fun five question yes or no guessing game for you so that you can learn even more about me!');
+
+/* ask the first yes or no question using a prompt*/
+var firstQuestion = prompt('Was I born in Renton, Washington? Please answer yes or no.');
+
+console.log(firsQuestion);
+
+/* put user response in correct format, y to YES, yes to YES, no to NO*/
+
+/* evaluate if the first question has been answered correctly, True, False,
+try again if wrong word has been entered */
+
+/* ask the second yes or no question */
+var firstQuestion = prompt('Do I have three college educated daughters? Please answer yes or no.');
+
+var firstQuestion = prompt('Do I enjoy wakeskating and karaoke? Please answer yes or no.');
+
+var firstQuestion = prompt('Is my favorite book "Wise Man" by Flannery OConnor? Please answer yes or no.');
+
+var firstQuestion = prompt('Did I once own a Suzuki GSXR 1000? Please answer yes or no.');
+/* if else statement redirecting to a sixth question Add an if/else statement
+to your javascript which redirects to a sixth question
+if and only if the answer is yes (or no, if that works better for you)*/
